276cf3c3a6aed86b37ea7de39adb94f610966b31
zakrnem <zakrnem@zohomail.com>
2023-07-05 22:48:37 -0600

    Implement insertShip function in progress

--- src/app/app.test.js
+++ src/app/app.test.js
@@ -1,5 +1,4 @@
-import { ship } from "./app";
-import { buildGrid } from "./app";
+import { ship, buildGrid, getPositionsArray, checkCoordinates } from "./app";
 
 const ship1 = ship(2)
 const ship2 = ship(5)
@@ -9,6 +8,19 @@ function executeMultipleTimes() {
     }
     return ship2.hit()
 }
+
+test(
+    `A ship with a length of 2 doesn't sinks on the first attack.`,
+    () => expect(ship1.hit()).toEqual(false))
+
+test(
+    'A ship with a length of 2 sinks on the second attack.',
+    () => expect(ship1.hit()).toEqual(true))
+
+test(
+    'A ship with a length of 5 sinks after 5 attacks.',
+    () => expect(executeMultipleTimes()).toEqual(true))
+
 const board = buildGrid()
 const boardResult = {
     1: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
@@ -21,22 +33,46 @@ const boardResult = {
     8: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
     9: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
     10: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-  }
+}
 
 test(
-    `A ship with a length of 2 doesn't sinks on the first attack`,
-    () => expect(ship1.hit()).toEqual(false))
-
-test(
-    'A ship with a length of 2 sinks on the second attack',
-    () => expect(ship1.hit()).toEqual(true))
-
-test(
-    'A ship with a length of 5 sinks after 5 attacks',
-    () => expect(executeMultipleTimes()).toEqual(true))
-
-test(
-    'The gameboard grid is built sucessfully',
+    'The gameboard grid is built sucessfully.',
     () => expect(board).toEqual(boardResult)
 )
 
+const start1 = ['B', 2]
+const shipLength1 = 5
+const positionsArray1 = [['B', 2], ['C', 2], ['D', 2], ['E', 2], ['F', 2]]
+const shipLength2 = 4
+const positionsArray2 = [['B', 2], ['B', 3], ['B', 4], ['B', 5]]
+
+test(
+    'Builds the ship positions array for horizontal orientation.',
+    () => expect(getPositionsArray(shipLength1, start1, 'horizontal')).toEqual(positionsArray1)
+)
+
+test(
+    'Builds the ship positions array for vertical orientation.',
+    () => expect(getPositionsArray(shipLength2, start1, 'vertical')).toEqual(positionsArray2)
+)
+
+/* test(
+    'Throws error when start position is invalid',
+    () => expect(getPositionsArray().toThrow(Error))
+) */
+
+test(
+    'Check coordinates function returns the same values when input is valid.',
+    () => {
+        expect(checkCoordinates('D', 10)).toEqual(['D', 10])
+        expect(checkCoordinates('A', 5)).toEqual(['A', 5])
+    }
+)
+
+test(
+    'Check coordinates function returns null when input is illegal.',
+    () => {
+        expect(checkCoordinates('D', 0)).toEqual(null);
+        expect(checkCoordinates('W', 4)).toEqual(null)
+    }
+)
