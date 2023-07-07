0e5a77608ec59bb9b9e244fae3ecc291066aebcf
zakrnem <zakrnem@zohomail.com>
2023-07-06 20:27:32 -0600

    Add checkCoordinates() tests and refactor all tests

--- src/app/app.js
+++ src/app/app.js
@@ -1,109 +1,123 @@
-import { lchown } from "fs"
-import { start } from "repl"
-
 export function ship(length) {
-    return {
-        length: length,
-        hits: 0,
-        hit: function () {
-            if (this.hits < this.length) ++this.hits
-            return this.isSunk()
-        },
-        isSunk: function () {
-            return (this.hits === this.length) ? true : false
-        }
-    }
+  return {
+    length: length,
+    hits: 0,
+    hit: function () {
+      if (this.hits < this.length) ++this.hits
+      return this.isSunk()
+    },
+    isSunk: function () {
+      return this.hits === this.length ? true : false
+    },
+  }
 }
 
 export function buildGrid() {
-    const boardObj = {}
+  const boardObj = {}
 
-    for (let row = 1; row <= 10; row++) {
-        boardObj[row] = {}
+  for (let row = 1; row <= 10; row++) {
+    boardObj[row] = {}
 
-        for (
-        let column = 'A';
-        column <= 'J';
-        column = String.fromCharCode(column.charCodeAt(0) + 1)
-        ) {
-        boardObj[row][column] = '_'
-        }
+    for (
+      let column = 'A';
+      column <= 'J';
+      column = String.fromCharCode(column.charCodeAt(0) + 1)
+    ) {
+      boardObj[row][column] = '_'
     }
-    return boardObj
+  }
+  return boardObj
 }
 
 export function gameboard() {
-    return {
-        grid: buildGrid(),
-        ship1: null,
-        ship2: null,
-        ship3: null,
-        ship4: null,
-        ship5: null,
-        receiveAttack: function () {
-            return this.allShipsSunk()
-        },
-        allShipsSunk: function () {
-
-        }
-    }
+  return {
+    grid: buildGrid(),
+    ship1: null,
+    ship2: null,
+    ship3: null,
+    ship4: null,
+    ship5: null,
+    receiveAttack: function () {
+      return this.allShipsSunk()
+    },
+    allShipsSunk: function () {},
+  }
 }
 
 function insertShip(shipNumber, startCoordinate, orientation) {
-    let length
-    
-    switch (true) {
-        case (shipNumber === 1):
-            length = 5
-            gameboard.ship1 = new ship(length)
-            break
-        case (shipNumber === 2):
-            length = 4
-            gameboard.ship2 = new ship(length)
-            break
-        case (shipNumber === 3):
-            length = 3
-            gameboard.ship3 = new ship(length)
-            break
-        case (shipNumber === 4):
-            length = 3
-            gameboard.ship4 = new ship(length)
-            break
-        case (shipNumber === 5):
-            length = 2
-            gameboard.ship5 = new ship(length)
-            break
-    }
+  let length
+
+  switch (true) {
+    case shipNumber === 1:
+      length = 5
+      gameboard.ship1 = new ship(length)
+      break
+    case shipNumber === 2:
+      length = 4
+      gameboard.ship2 = new ship(length)
+      break
+    case shipNumber === 3:
+      length = 3
+      gameboard.ship3 = new ship(length)
+      break
+    case shipNumber === 4:
+      length = 3
+      gameboard.ship4 = new ship(length)
+      break
+    case shipNumber === 5:
+      length = 2
+      gameboard.ship5 = new ship(length)
+      break
+  }
 }
 
 export function getPositionsArray(length, startCoordinate, orientation) {
-    const positionsArray = []
-    const coordinates = checkCoordinates(startCoordinate[0], startCoordinate[1])
+  const positionsArray = []
+  let coordinates = checkCoordinates(startCoordinate[0], startCoordinate[1])
+  if (coordinates === null) {
+    throw new Error('Start position is outside of board')
+  } else {
     let letterCoordinate = coordinates[0]
     let numberCoordinate = coordinates[1]
 
     if (orientation === 'horizontal') {
-        for (let i = 0; i < length; i++) {
-            if (i === 0) positionsArray.push([letterCoordinate, numberCoordinate])
-            else {
-                letterCoordinate = String.fromCharCode(letterCoordinate[0].charCodeAt(0) + 1)
-                positionsArray.push([letterCoordinate, numberCoordinate])
-            }
+      for (let i = 0; i < length; i++) {
+        if (i === 0) positionsArray.push([letterCoordinate, numberCoordinate])
+        else {
+          letterCoordinate = String.fromCharCode(
+            letterCoordinate[0].charCodeAt(0) + 1
+          )
+          coordinates = checkCoordinates(letterCoordinate, numberCoordinate)
+          positionsArray.push(coordinates)
         }
+      }
     } else if (orientation === 'vertical') {
-        for (let i = 0; i < length; i++) {
-            if (i === 0) positionsArray.push([letterCoordinate, numberCoordinate])
-            else {
-                numberCoordinate += 1
-                positionsArray.push([letterCoordinate, numberCoordinate])
-            }
+      for (let i = 0; i < length; i++) {
+        if (i === 0) positionsArray.push([letterCoordinate, numberCoordinate])
+        else {
+          numberCoordinate += 1
+          coordinates = checkCoordinates(letterCoordinate, numberCoordinate)
+          positionsArray.push(coordinates)
         }
+      }
     }
-    return positionsArray
+
+    if (coordinates === null) {
+      throw new Error('Part of the ship is outside the board')
+    } else {
+      return positionsArray
+    }
+  }
 }
 
 export function checkCoordinates(letterCoordinate, numberCoordinate) {
-    letterCoordinate = letterCoordinate.charCodeAt() < 65 || letterCoordinate.charCodeAt() > 74 ? null : letterCoordinate
-    numberCoordinate = numberCoordinate > 10 || numberCoordinate < 1 ? null : numberCoordinate
-    return letterCoordinate === null || numberCoordinate === null ? null : [letterCoordinate, numberCoordinate]
-  }
+  letterCoordinate =
+    letterCoordinate.charCodeAt() < 65 || letterCoordinate.charCodeAt() > 74
+      ? null
+      : letterCoordinate
+  numberCoordinate =
+    numberCoordinate > 10 || numberCoordinate < 1 ? null : numberCoordinate
+  return letterCoordinate === null || numberCoordinate === null
+    ? null
+    : [letterCoordinate, numberCoordinate]
+}
--- src/app/app.test.js
+++ src/app/app.test.js
@@ -1,78 +1,195 @@
-import { ship, buildGrid, getPositionsArray, checkCoordinates } from "./app";
+import { ship, buildGrid, getPositionsArray, checkCoordinates } from './app'
 
-const ship1 = ship(2)
-const ship2 = ship(5)
-function executeMultipleTimes() {
-    for(let i = 0; i < 4; i++) {
-        ship2.hit()
+describe('ship Factory', () => {
+  const ship1 = ship(2)
+  const ship2 = ship(5)
+  function executeMultipleTimes() {
+    for (let i = 0; i < 4; i++) {
+      ship2.hit()
     }
     return ship2.hit()
-}
+  }
+  test(`A ship with a length of 2 doesn't sinks on the first attack.`, () =>
+    expect(ship1.hit()).toEqual(false))
 
-test(
-    `A ship with a length of 2 doesn't sinks on the first attack.`,
-    () => expect(ship1.hit()).toEqual(false))
+  test('A ship with a length of 2 sinks on the second attack.', () =>
+    expect(ship1.hit()).toEqual(true))
 
-test(
-    'A ship with a length of 2 sinks on the second attack.',
-    () => expect(ship1.hit()).toEqual(true))
+  test('A ship with a length of 5 sinks after 5 attacks.', () =>
+    expect(executeMultipleTimes()).toEqual(true))
+})
 
-test(
-    'A ship with a length of 5 sinks after 5 attacks.',
-    () => expect(executeMultipleTimes()).toEqual(true))
+describe('buildGrid()', () => {
+  const board = buildGrid()
+  const boardResult = {
+    1: {
+      A: '_',
+      B: '_',
+      C: '_',
+      D: '_',
+      E: '_',
+      F: '_',
+      G: '_',
+      H: '_',
+      I: '_',
+      J: '_',
+    },
+    2: {
+      A: '_',
+      B: '_',
+      C: '_',
+      D: '_',
+      E: '_',
+      F: '_',
+      G: '_',
+      H: '_',
+      I: '_',
+      J: '_',
+    },
+    3: {
+      A: '_',
+      B: '_',
+      C: '_',
+      D: '_',
+      E: '_',
+      F: '_',
+      G: '_',
+      H: '_',
+      I: '_',
+      J: '_',
+    },
+    4: {
+      A: '_',
+      B: '_',
+      C: '_',
+      D: '_',
+      E: '_',
+      F: '_',
+      G: '_',
+      H: '_',
+      I: '_',
+      J: '_',
+    },
+    5: {
+      A: '_',
+      B: '_',
+      C: '_',
+      D: '_',
+      E: '_',
+      F: '_',
+      G: '_',
+      H: '_',
+      I: '_',
+      J: '_',
+    },
+    6: {
+      A: '_',
+      B: '_',
+      C: '_',
+      D: '_',
+      E: '_',
+      F: '_',
+      G: '_',
+      H: '_',
+      I: '_',
+      J: '_',
+    },
+    7: {
+      A: '_',
+      B: '_',
+      C: '_',
+      D: '_',
+      E: '_',
+      F: '_',
+      G: '_',
+      H: '_',
+      I: '_',
+      J: '_',
+    },
+    8: {
+      A: '_',
+      B: '_',
+      C: '_',
+      D: '_',
+      E: '_',
+      F: '_',
+      G: '_',
+      H: '_',
+      I: '_',
+      J: '_',
+    },
+    9: {
+      A: '_',
+      B: '_',
+      C: '_',
+      D: '_',
+      E: '_',
+      F: '_',
+      G: '_',
+      H: '_',
+      I: '_',
+      J: '_',
+    },
+    10: {
+      A: '_',
+      B: '_',
+      C: '_',
+      D: '_',
+      E: '_',
+      F: '_',
+      G: '_',
+      H: '_',
+      I: '_',
+      J: '_',
+    },
+  }
+  test('The gameboard grid is built sucessfully.', () =>
+    expect(board).toEqual(boardResult))
+})
 
-const board = buildGrid()
-const boardResult = {
-    1: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-    2: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-    3: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-    4: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-    5: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-    6: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-    7: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-    8: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-    9: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-    10: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-}
+describe('checkCoordinates()', () => {
+  test('Check coordinates function returns the same values when input is valid.', () => {
+    expect(checkCoordinates('D', 10)).toEqual(['D', 10])
+    expect(checkCoordinates('A', 5)).toEqual(['A', 5])
+  })
 
-test(
-    'The gameboard grid is built sucessfully.',
-    () => expect(board).toEqual(boardResult)
-)
+  test('Check coordinates function returns null when input is illegal.', () => {
+    expect(checkCoordinates('D', 0)).toEqual(null)
+    expect(checkCoordinates('W', 4)).toEqual(null)
+    expect(checkCoordinates('A', 13)).toEqual(null)
+    expect(checkCoordinates('F', 12)).toEqual(null)
+  })
+})
 
-const start1 = ['B', 2]
-const shipLength1 = 5
-const positionsArray1 = [['B', 2], ['C', 2], ['D', 2], ['E', 2], ['F', 2]]
-const shipLength2 = 4
-const positionsArray2 = [['B', 2], ['B', 3], ['B', 4], ['B', 5]]
-
-test(
-    'Builds the ship positions array for horizontal orientation.',
-    () => expect(getPositionsArray(shipLength1, start1, 'horizontal')).toEqual(positionsArray1)
-)
-
-test(
-    'Builds the ship positions array for vertical orientation.',
-    () => expect(getPositionsArray(shipLength2, start1, 'vertical')).toEqual(positionsArray2)
-)
-
-/* test(
-    'Throws error when start position is invalid',
-    () => expect(getPositionsArray().toThrow(Error))
-) */
-
-test(
-    'Check coordinates function returns the same values when input is valid.',
-    () => {
-        expect(checkCoordinates('D', 10)).toEqual(['D', 10])
-        expect(checkCoordinates('A', 5)).toEqual(['A', 5])
-    }
-)
-
-test(
-    'Check coordinates function returns null when input is illegal.',
-    () => {
-        expect(checkCoordinates('D', 0)).toEqual(null);
-        expect(checkCoordinates('W', 4)).toEqual(null)
-    }
-)
+describe('getPositionsArray()', () => {
+  const start1 = ['B', 2]
+  const shipLength1 = 5
+  const positionsArray1 = [
+    ['B', 2],
+    ['C', 2],
+    ['D', 2],
+    ['E', 2],
+    ['F', 2],
+  ]
+  const shipLength2 = 4
+  const positionsArray2 = [
+    ['B', 2],
+    ['B', 3],
+    ['B', 4],
+    ['B', 5],
+  ]
+  const start2 = ['B', 10]
+  test('Builds the ship positions array for horizontal orientation.', () =>
+    expect(getPositionsArray(shipLength1, start1, 'horizontal')).toEqual(
+      positionsArray1
+    ))
+  test('Builds the ship positions array for vertical orientation.', () =>
+    expect(getPositionsArray(shipLength2, start1, 'vertical')).toEqual(
+      positionsArray2
+    ))
+  test('throws error when part of the ship is outside the board', () => {
+    expect(() => {
+      getPositionsArray(shipLength1, start2, 'vertical')
+    }).toThrow('Part of the ship is outside the board')
+  })
+})
