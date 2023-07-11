072df2e8970d588fcc0e1cfb813f61550ecd5fe9
zakrnem <zakrnem@zohomail.com>
2023-07-10 20:38:22 -0600

    Refactor test in separate files

--- src/app/gameObj.test.js
+++ src/app/gameObj.test.js
@@ -3,7 +3,6 @@ import {
   gameboard,
 } from './gameObj'
 
-import { checkCoordinates } from './checkCoordinates'
 import { insertShip } from './insertShip'
 
 describe('ship Factory', () => {
@@ -23,20 +22,6 @@ describe('ship Factory', () => {
 
   test('A ship with a length of 5 sinks after 5 attacks.', () =>
     expect(executeMultipleTimes()).toEqual(true))
-})
-
-describe('checkCoordinates: Checks whether a specific coordinate is inside the board.', () => {
-  test('Check coordinates function returns the same values when input is valid.', () => {
-    expect(checkCoordinates('D', 10)).toEqual(['D', 10])
-    expect(checkCoordinates('A', 5)).toEqual(['A', 5])
-  })
-
-  test('Check coordinates function returns null when input is illegal.', () => {
-    expect(checkCoordinates('D', 0)).toEqual(null)
-    expect(checkCoordinates('W', 4)).toEqual(null)
-    expect(checkCoordinates('A', 13)).toEqual(null)
-    expect(checkCoordinates('F', 12)).toEqual(null)
-  })
 })
 
 describe('gameboard: A empty board receives attacks.', () => {
@@ -418,52 +403,6 @@ describe('gameboard: A empty board receives attacks.', () => {
   })
   test('Using the method locateShip() inserts one block of a ship in one cell of the board.', () => {
     expect(game.locateShipCell(shipNumber, shipCoordinate)).toEqual(grid3)
-  })
-})
-
-describe('insertShip: Inserts ships on the board.', () => {
-  const game = gameboard()
-  const startCoordinate1 = ['B', 6]
-  const orientation1 = 'horizontal'
-  const startCoordinate2 = ['E', 9]
-  const orientation2 = 'horizontal'
-  const startCoordinate3 = ['C', 1]
-  const orientation3 = 'vertical'
-  const startCoordinate4 = ['D', 8]
-  const orientation4 = 'vertical'
-  const startCoordinate5 = ['I', 4]
-  const orientation5 = 'vertical'
-  const grid = {
-    1: { A: '_', B: '_', C: '3', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-    2: { A: '_', B: '_', C: '3', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-    3: { A: '_', B: '_', C: '3', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-    4: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '5', J: '_' },
-    5: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '5', J: '_' },
-    6: { A: '_', B: '1', C: '1', D: '1', E: '1', F: '1', G: '_', H: '_', I: '_', J: '_' },
-    7: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-    8: { A: '_', B: '_', C: '_', D: '4', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-    9: { A: '_', B: '_', C: '_', D: '4', E: '2', F: '2', G: '2', H: '2', I: '_', J: '_' },
-    10: { A: '_', B: '_', C: '_', D: '4', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
-  }
-  insertShip(game, 1, startCoordinate1, orientation1)
-  insertShip(game, 2, startCoordinate2, orientation2)
-  insertShip(game, 3, startCoordinate3, orientation3)
-  insertShip(game, 4, startCoordinate4, orientation4)
-  insertShip(game, 5, startCoordinate5, orientation5)
-  test('Locates multiple ships on the board.', () => {
-    expect(game.grid).toEqual(grid)
-  })
-  test(`Assigns node values to the game obj parameter 'ship1'.`, () => {
-    expect(game.ship1.length).toEqual(5)
-  })
-  test(`Assigns node values to the game obj parameter 'ship2'.`, () => {
-    expect(game.ship2.length).toEqual(4)
-  })
-  test(`Assigns node values to the game obj parameter 'ship3'.`, () => {
-    expect(game.ship3.length).toEqual(3)
-  })
-  test(`Assigns node values to the game obj parameter 'ship4'.`, () => {
-    expect(game.ship4.length).toEqual(3)
   })
 })
 
---
+++ src/app/insertShip.test.js
@@ -0,0 +1,48 @@
+import { insertShip } from './insertShip'
+import { gameboard } from './gameObj'
+
+describe('insertShip: Inserts ships on the board.', () => {
+    const game = gameboard()
+    const startCoordinate1 = ['B', 6]
+    const orientation1 = 'horizontal'
+    const startCoordinate2 = ['E', 9]
+    const orientation2 = 'horizontal'
+    const startCoordinate3 = ['C', 1]
+    const orientation3 = 'vertical'
+    const startCoordinate4 = ['D', 8]
+    const orientation4 = 'vertical'
+    const startCoordinate5 = ['I', 4]
+    const orientation5 = 'vertical'
+    const grid = {
+      1: { A: '_', B: '_', C: '3', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
+      2: { A: '_', B: '_', C: '3', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
+      3: { A: '_', B: '_', C: '3', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
+      4: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '5', J: '_' },
+      5: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '5', J: '_' },
+      6: { A: '_', B: '1', C: '1', D: '1', E: '1', F: '1', G: '_', H: '_', I: '_', J: '_' },
+      7: { A: '_', B: '_', C: '_', D: '_', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
+      8: { A: '_', B: '_', C: '_', D: '4', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
+      9: { A: '_', B: '_', C: '_', D: '4', E: '2', F: '2', G: '2', H: '2', I: '_', J: '_' },
+      10: { A: '_', B: '_', C: '_', D: '4', E: '_', F: '_', G: '_', H: '_', I: '_', J: '_' },
+    }
+    insertShip(game, 1, startCoordinate1, orientation1)
+    insertShip(game, 2, startCoordinate2, orientation2)
+    insertShip(game, 3, startCoordinate3, orientation3)
+    insertShip(game, 4, startCoordinate4, orientation4)
+    insertShip(game, 5, startCoordinate5, orientation5)
+    test('Locates multiple ships on the board.', () => {
+      expect(game.grid).toEqual(grid)
+    })
+    test(`Assigns node values to the game obj parameter 'ship1'.`, () => {
+      expect(game.ship1.length).toEqual(5)
+    })
+    test(`Assigns node values to the game obj parameter 'ship2'.`, () => {
+      expect(game.ship2.length).toEqual(4)
+    })
+    test(`Assigns node values to the game obj parameter 'ship3'.`, () => {
+      expect(game.ship3.length).toEqual(3)
+    })
+    test(`Assigns node values to the game obj parameter 'ship4'.`, () => {
+      expect(game.ship4.length).toEqual(3)
+    })
+  })
