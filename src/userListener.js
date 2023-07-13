0220b65efa7b4d99d9fc7b2dddc5e622e0362df8
zakrnem <zakrnem@zohomail.com>
2023-07-12 18:56:24 -0600

    Change cell color on mouse hover

--- src/userListener.js
+++ src/userListener.js
@@ -1,11 +1,11 @@
 export function userBoardListener() {
     const board = document.querySelector('.user-board')
     board.addEventListener('mouseover', (e) => {
-        e.target.style.transition = "background-color 0.5s ease-in"
-        e.target.style.backgroundColor = '#2b2b1a'
+        e.target.style.transition = "background-color 0.2s ease-in"
+        e.target.style.backgroundColor = '#d4d4c0'
         setTimeout(() => {
-            e.target.style.transition = "background-color 0.5s ease-in"
+            e.target.style.transition = "background-color 0.2s ease-in"
             e.target.style.backgroundColor = ''
-        }, 500)
+        }, 600)
     })
 }
