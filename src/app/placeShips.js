4d2d0235a3ed6e5210752de32076cc7dd1ca38c2
zakrnem <zakrnem@zohomail.com>
2023-07-19 19:53:54 -0600

    Places ships on the DOM sucessfully

--- src/app/placeShips.js
+++ src/app/placeShips.js
@@ -1,32 +1,51 @@
-
-
 export function placeShipListener() {
     const userBoard = document.querySelector('.user-board')
-    userBoard.addEventListener('mouseover', (e) => placeShipTemp(e))
-    userBoard.addEventListener('mouseleave', (e) => removeShipTemp())
-    //userBoard.addEventListener('click', (e) => placeShip(e))
+    userBoard.addEventListener('click', (e) => placeShip(e))
+    userBoard.addEventListener('mouseover', (e) => placeShipTemp(e))   
 }
 
 const ships = [5, 4, 3, 3, 2]
 let tempCells = []
 
 function placeShipTemp(e) {
-    removeShipTemp()
-    const currLength = ships[0]
-    const startID = e.target.id
-    let startIdNumber = startID.match(/\d+/g)
+    if (e.target.id !== 'occupied') {
+        removeShipTemp(e)
+        const currLength = ships[0]
+        const startID = e.target.id
+        let startIdNumber = startID.match(/\d+/g)
 
-    tempCells = []
-    tempCells.push(startID)
-    
-    for (let i = 1; i < currLength; i++) {
-        const cellID = 'user' + ++startIdNumber
-        tempCells.push(cellID)
-        let cell = document.getElementById(cellID)
-        cell.style.backgroundColor = 'var(--ship-color)'
+        tempCells = []
+        tempCells.push(startID)
+        
+        for (let i = 1; i < currLength; i++) {
+            const cellID = 'user' + ++startIdNumber
+            tempCells.push(cellID)
+            let cell = document.getElementById(cellID)
+            if(cell !== null) {
+                cell.style.backgroundColor = 'var(--mouseover)'
+            }
+        }
+        e.target.style.backgroundColor = 'var(--mouseover)'
     }
+}
 
-    e.target.style.backgroundColor = 'var(--ship-color)'
+function placeShip(e) {
+    if (e.target.id !== 'occupied' &&
+    ships.length > 0) {
+        const currLength = ships.shift()
+        const startID = e.target.id
+        let startIdNumber = startID.match(/\d+/g)
+
+        for (let i = 1; i < currLength; i++) {
+            const cellID = 'user' + ++startIdNumber
+            let cell = document.getElementById(cellID)
+            cell.id = 'occupied'
+            cell.style.backgroundColor = 'var(--ship-color)'
+        }
+        let startCell = document.getElementById(startID)
+        startCell.id = 'occupied'
+        e.target.style.backgroundColor = 'var(--ship-color)'
+    }
 }
 
 function removeShipTemp() {
@@ -34,6 +53,8 @@ function removeShipTemp() {
     for (let i = 0; i < length; i++) {
         const cellID = tempCells.shift()
         let cell = document.getElementById(cellID)
-        cell.style.backgroundColor = ''
+        if(cell !== null) {
+            cell.style.backgroundColor = ''
+        }
     }
 }
