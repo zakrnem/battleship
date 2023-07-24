b2f0db88384698c00586009a393fc5022ceedda3
zakrnem <zakrnem@zohomail.com>
2023-07-23 22:34:11 -0600

    Implementation of reportNumberOfAttacks() in progress

--- src/app/reportAllShipsSunk.js
+++ src/app/reportAllShipsSunk.js
@@ -1,4 +1,7 @@
 import { boardDisabler } from './boardDisabler'
+import { game } from './game'
+import { arrayFromBoard } from './printBoard'
+
 
 export function reportAllShipsSunk(board) {
   const boolean = board.allShipsSunk()
@@ -6,5 +9,25 @@ export function reportAllShipsSunk(board) {
     const pcMessage = document.getElementById('pc-message')
     pcMessage.textContent = 'All ships have sunk'
     boardDisabler('write')
+    reportNumberOfAttacks()
   }
+}
+
+function reportNumberOfAttacks() {
+  let gameboard = game('return-board')
+
+  let userArray = arrayFromBoard(gameboard.user.grid)
+  let userAttacks = 0
+  userArray.forEach((el) => {
+    if (el === 'X') userAttacks++
+  })
+
+  let computerArray = arrayFromBoard(gameboard.computer.grid)
+  let computerAttacks = 0
+  computerArray.forEach((el) => {
+    if (el === 'X') computerAttacks++
+  })
+
+  console.log('user: ' + userAttacks + ', computer: ' + computerAttacks)
+  console.log('userArr: ', userArray, ' computerArr: ', computerArray)
 }
